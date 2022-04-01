3cc26c4eeecfc1169fffb9dbb56f23e6b8490775
Thomas Petiteau <thomas.petiteau@outlook.com>
2022-04-02 00:19:49 +0200

    mind node widget display mind node text

--- xindmap/widget/MindNodeWidget.py
+++ xindmap/widget/MindNodeWidget.py
@@ -5,12 +5,14 @@ class MindNodeWidget(kwidget.Widget):
     """widget used to display a mind node
 
     Attributes:
+        label: a label used to display the text of the mind node
         mind_node: the mind node displayed
         mind_node_widget_children: the list of mind node widget that display the
             children of the mind node
         mind_node_widget_parent: the mind node widget that display the parent of
             the mind node
     """
+    label = kproperties.ObjectProperty()
     mind_node_widget_children = kproperties.ListProperty()
     mind_node_widget_parent = kproperties.ObjectProperty()
 
@@ -26,3 +28,12 @@ class MindNodeWidget(kwidget.Widget):
         super().__init__(**kwargs)
 
         self.mind_node = mind_node
+
+    def on_mind_node_text(self, mind_node, text):
+        """callback raised upon changing the text of the mind node
+
+        Args:
+            mind_node: the mind node
+            text: the new text of the mind node
+        """
+        self.label.text = text
